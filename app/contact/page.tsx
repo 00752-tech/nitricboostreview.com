a982958692c3b3953717401b31509422fe761b07
Abdun Noor Faruki Biswas <dev.abdunnoor@gmail.com>
2025-01-12 06:49:39 +0530

    Change emaill address form contact page.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
         to reach out to us using the information below:`}
       </p>
       <ul className="list-disc pl-6 mb-4">
-        <li>Email: https://nitricboostreview.com/</li>
+        <li>Email: passiveincomestreaming@gmail.com</li>
       </ul>
       <p className="mb-4">
         We strive to respond to all inquiries within 24-48 hours during business
